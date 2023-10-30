30cc0b2610016f76e33b02e6ad65015f36d2ff29
Guilherme de Azevedo <gui-azesantos@hotmail.com>
2023-10-30 17:45:46 -0300

    fix(build): fixing build errors

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,4 @@
-import { VStack } from "@chakra-ui/react";
+
 import styled from "styled-components";
 
 export const CustomVStack = styled.div`
--- src/pages/Hobbies/Hobbies.tsx
+++ src/pages/Hobbies/Hobbies.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { useForm, Controller } from 'react-hook-form';
 import {
   FormControl,
